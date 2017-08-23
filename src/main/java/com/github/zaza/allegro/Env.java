143b0250f677a3c382432174ddf174e13204b80a
Tomasz Zarna <tzarna@gmail.com>
2017-08-23 23:34:13 +0200

    initial commit, extracted from allegro-rss

---
+++ src/main/java/com/github/zaza/allegro/Env.java
@@ -0,0 +1,7 @@
+package com.github.zaza.allegro;
+
+public final class Env {
+	public static final String ALLEGRO_LOGIN = "ALLEGRO_LOGIN";
+	public static final String ALLEGRO_PASSWORD = "ALLEGRO_PASSWORD";
+	public static final String ALLEGRO_WEBAPI_KEY = "ALLEGRO_WEBAPI_KEY";
+}
---
+++ src/main/java/com/github/zaza/allegro/Filter.java
@@ -0,0 +1,18 @@
+package com.github.zaza.allegro;
+
+public class Filter {
+	
+	// TODO quick and dirty replacement for spark.Request
+
+	public String string;
+	
+	public String price_from;
+	
+	public String price_to;
+	
+	public String buyUsed;
+	
+	public String buyNew;
+	
+	public String category;
+}
